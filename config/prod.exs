8f76843f455eec70550f4d070a684b16220e366c
RJ Dellecese <rj.dellecese@annkissam.com>
2017-02-25 23:20:34 -0500

    Add prod database config and use secret env var

--- config/prod.exs
+++ config/prod.exs
@@ -14,10 +14,18 @@ use Mix.Config
 config :hon_graffiti_phoenix, HonGraffitiPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :hon_graffiti_phoenix, HonGraffitiPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
