f24efe5fd81d04bbb33410b1a1d5ecd4172f7dba
Aaron Votre <avotre422@gmail.com>
2017-03-12 17:50:02 -0400

    Update tests to 4phase

--- test/lib/markdown_parser_test.exs
+++ test/lib/markdown_parser_test.exs
@@ -2,60 +2,79 @@ defmodule HonGraffitiPhoenix.MarkupParserTest do
   use HonGraffitiPhoenix.ConnCase
   alias HonGraffitiPhoenix.Parsers.MarkupParser
 
-
-  @quote "^rBold ^gWow ^*Nein"
-  @no_markdown "thisIsSoFakn' BORING"
-  @broken_markdown "this^Qmakes no ^09s sense!!"
-  @invalid_markup_code "^j doesn't correspond to a code"
-  @caret_not_first "this is a ^rrandom string"
-  @multiple_carets "We shouldn't freak over ^^^^^too many carets"
-
-
-  test "it returns the original string if there is no style" do
-    parsed = MarkupParser.parse(@no_markdown)
-    assert length(parsed) == 1
-    assert hd(parsed).body == @no_markdown
-  end
-
-  test "it returns the caret if the style code is invalid" do
-    parsed = MarkupParser.parse(@invalid_markup_code)
-    assert length(parsed) == 1
-    assert hd(parsed).body == @invalid_markup_code
-  end
-
-  test "it parses strings that don't start with carets" do
-    parsed = MarkupParser.parse(@caret_not_first)
-    assert length(parsed) == 2
-    assert hd(parsed).body == "this is a "
-  end
-
   test "parsing an empty quote returns an empty list" do
     assert length(MarkupParser.parse()) == 0
   end
 
-  test "it returns an element for each valid style code" do
-    assert length(MarkupParser.parse(@quote)) == 3
+  test "it returns the original string if there is no style" do
+    no_markup = "thisIsSoFakn' BORING"
+
+    parsed = MarkupParser.parse(no_markup)
+
+    assert length(parsed) == 1
+    assert hd(parsed).body == no_markup
+  end
+
+  test "it returns the caret if the style code is invalid" do
+    invalid_markup = "^j doesn't correspond to a code"
+
+    parsed = MarkupParser.parse(invalid_markup)
+
+    assert length(parsed) == 1
+    assert hd(parsed).body == invalid_markup
+  end
+
+  test "it parses strings that don't start with carets" do
+    caret_anywhere = "this is a ^rrandom string"
+
+    parsed = MarkupParser.parse(caret_anywhere)
+
+    assert length(parsed) == 2
+    assert hd(parsed).body == "this is a "
+  end
+
+  test "it parses multiple carets of valid markup" do
+    valid_quote = "^rBold ^gWow ^*Nein"
+
+    parsed = MarkupParser.parse(valid_quote)
+
+    assert length(parsed) == 3
+    assert hd(parsed).body == "Bold "
+    assert hd(parsed).color == "red"
+    assert List.last(parsed).body == "Nein"
+    assert List.last(parsed).color == "white"
   end
 
   test "it will default to white if there is no valid markup" do
-    markdown_data = MarkupParser.parse(@broken_markdown)
-    color_array = Enum.reduce(markdown_data, [], fn(color_struct, acc) -> [Map.fetch!(color_struct, :color) | acc] end)
-    assert Enum.all?(color_array, fn(x) -> x == "white" end)
+    invalid_markup = "this^Qmakes no ^09s sense!!"
+
+    parsed = MarkupParser.parse(invalid_markup)
+
+    assert Enum.all?(parsed, fn(decorated_string) -> Map.fetch!(decorated_string, :color) == "white" end)
   end
 
-  test "it can parse 3 digit codes or single characters" do
-    assert hd(MarkupParser.parse("^000")).color == "rgb(0,0,0)"
-    assert hd(MarkupParser.parse("^123")).color == "rgb(28,56,84)"
-    assert hd(MarkupParser.parse("^r")).color == "red"
-    assert hd(MarkupParser.parse("^*")).color == "white"
+  test "it parses colors from markup codes and digits" do
+    rgb_color = "^123words"
+    code_color = "^rwords"
+
+    rgb_parsed = MarkupParser.parse(rgb_color)
+    code_parsed = MarkupParser.parse(code_color)
+
+    assert hd(rgb_parsed).color == "rgb(28,56,84)"
+    assert hd(code_parsed).color == "red"
   end
 
-  test "it can handle multiple carets" do
-    parsed = MarkupParser.parse(@multiple_carets)
-    assert length(parsed) == 2
-    assert hd(parsed).body == "We shouldn't freak over ^^^^"
-    assert hd(parsed).color == "white"
-    assert List.last(parsed).color == "teal"
+  test "it groups multiple sequential carets into one body, except the last one" do
+    valid_multiple_carets = "Don't freak^^^^^too many carets"
+    invalid_multiple_carets = "Don't freak^^^^^1too many carets"
+
+    valid_parsed = MarkupParser.parse(valid_multiple_carets)
+    invalid_parsed = MarkupParser.parse(invalid_multiple_carets)
+
+    assert List.last(valid_parsed).body == "oo many carets"
+    assert List.last(valid_parsed).color == "teal"
+    assert hd(invalid_parsed).body == "Don't freak^^^^"
+    assert hd(invalid_parsed).color == "white"
   end
 
 end
